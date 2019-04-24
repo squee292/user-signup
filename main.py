a0e892cdd84fde74078ca0c8554dde8e5d80a489
Jesse Clark <squee292darkside@jesses-air.southwest.tn.edu>
2019-04-24 17:46:05 -0500

    updated code

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from flask import Flask, request, redirect, render_template
-import cgi
-import os
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-def is_length():
-    test_lenght = request.form('username')
-    if len(test_lenght) < 3 or len(test_lenght) > 20:
-        return False
-    else:
+def is_length(text):
+    len_check = len(text)
+    if len_check >= 3 and len_check <= 20:
         return True
+    else:
+        return False
 
 
 
@@ -21,10 +20,14 @@ def index():
 
 
 
-@app.route("/", methods = ['POST'])
+@app.route("/validate", methods = ['POST'])
 def user_login():
-    if is_length():
-        return render_template('signup.html', title = "Signup", username_error = "Please enter a user name between 3-20 char long")
-    else: 
-        return <h1> Correct </h1>
+    
+    test_lenght = request.form['username']
+    username_error = ''
+
+    if not is_length(test_length):
+        username_error = "Please enter a user name between 3-20 char long"
+        return render_template('signup.html', title = "Signup", username_error = username_error)
+    
 app.run()
