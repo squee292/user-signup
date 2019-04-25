3a71b37ae2abbeaecd9c6432882676da4387c8e2
Jesse Clark <squee292darkside@jesses-air.southwest.tn.edu>
2019-04-24 20:29:59 -0500

    updated files

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ app.config['DEBUG'] = True
 
 def is_length(text):
     len_check = len(text)
-    if len_check >= 3 and len_check <= 20:
-        return True
-    else:
+    if len_check < 3 or len_check > 20:
         return False
+    else:
+        return True
 
 
 
@@ -20,14 +20,14 @@ def index():
 
 
 
-@app.route("/validate", methods = ['POST'])
+@app.route("/", methods=['POST'])
 def user_login():
-    
-    test_lenght = request.form['username']
+
+    test_length = request.form["username"]
     username_error = ''
 
-    if not is_length(test_length):
+    if not is_length(test_length):    
         username_error = "Please enter a user name between 3-20 char long"
-        return render_template('signup.html', title = "Signup", username_error = username_error)
+        return render_template('signup.html', username_error = username_error)
     
 app.run()
