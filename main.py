b95fcfc124456b8ff8753a35b0a6ed6f3bb88f54
Jesse Clark <squee292darkside@Jesses-MacBook-Air.local>
2019-04-28 17:49:08 -0500

    updated files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 import string
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -11,14 +12,18 @@ def is_length(text):
     else:
         return True
 
-
-
-
 def is_same(text1, text2):
     if text1 != text2:
         return False
     else:
         return True
+
+def validate_email(email):
+    if len(email) >= 3:
+        if re.search("[@.]", email) != None:
+            return True
+    else:    
+        return False
 
 
 
@@ -47,6 +52,7 @@ def user_login():
     error_control = 0
 
 
+
     if not is_length(test_username):    
         username_error = "Please enter a user name between 3-20 char long"
         username = ''
@@ -64,23 +70,28 @@ def user_login():
     if not is_length(test_password1):
         password_error = "Please enter a password between 3-20 char long"
         error_control += 1
+
+    if test_password1.isspace():
+        password_error = "Please enter a password with no spaces"
+        error_control += 1
     
     if not is_same(test_password1, test_password2):
         password_error_two = "Passwords do not match"
         error_control += 1
     
-    #if test_email = '':
-        #email = test_email
-    #else:     
-        #if not email_check(test_email):
-            #email_error = "Please enter a valid email"
-        #else: 
-            #email = str(test_email)
+    if validate_email(test_email) == True:
+        email = test_email
+    else:
+        email_error = "Please enter a valid email"
+        error_control += 1
+
+    
 
 
     if error_control > 0:
-        return render_template('signup.html', title = 'Signup', username_error = username_error,
-        password_error = password_error, password_error_two = password_error_two)
+        return render_template('signup.html', title = 'Signup', username = username, 
+        email = email , username_error = username_error, password_error = password_error,
+        password_error_two = password_error_two)
     else:
         return render_template('welcome.html', title = 'Welcome')
 app.run()
